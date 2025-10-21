ccec80ccc362cb39fe92b99c22337d7c8f2208cb
ij93 <alazawy.m91@gmail.com>
2025-10-21 08:57:07 +0200

    Create a Bord class to manage the Minesweeper grid, and added its Constructor.

--- src/Bord.java
+++ src/Bord.java
@@ -1,2 +1,19 @@
+//The Board class manages the Minesweeper grid.
 public class Bord {
+    private final int rows;
+    private final int cols;
+    private final int totalMines;
+    private final Cell[][] grid; // 2D array of Cell objects
+
+    //Constructor: creates a board of given size with given number of mines.
+    public Bord(int rows, int cols,  int totalMines) {
+        this.rows = rows;
+        this.cols = cols;
+        this.totalMines = totalMines;
+        grid = new Cell[rows][cols];
+    }
+
+
+
+
 }
