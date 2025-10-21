2cdcfea42a38c872e8aede15b93bcaeaddbfea14
Micke <mikael.dramatic@gmail.com>
2025-10-21 11:36:16 +0200

    Starting GameController.java.
    Finished the coding on GameController.java.
    
    Some minor errors because player.java hasn't start coding yet. HighScore up next

--- src/GameController.java
+++ src/GameController.java
@@ -1,7 +1,73 @@
 public class GameController {
+    private Board board;
+    private Player player;
+    private CpuUser cpu;
+    private boolean isRunning;
+    private HighScore highscore;
 
+    public GameController() {
+        this.board= new Board(10,10,10);
+        this.player = new Player ("Player 1");
+        this.cpu = new CpuUser("CPU");
+        this.isRunning = true;
+        this.highscore = new HighScore();
+    }
 
-//To do
-//
-//
+    public void startGame() {
+        IO.println("Welcome to Minesweeper!");
+        board.printBoard(false);
+
+        while (isRunning) {
+            playerTurn();
+            if (checkGameOver()) break;
+
+            cpuTurn();
+            if (checkGameOver()) break;
+        }
+
+        endGame();
+    }
+//Check if code works with r and c
+
+    private void playerTurn() {
+        IO.println(String.format("Players turn!" , player.getName()));
+        int row = IO.readInt ("Choose Row: ");
+        int col = IO.readInt ("Choose Colon: ");
+
+        if (board.revealCell(row, col)) {
+            IO.println("BOOM, the bomb exploded!");
+            isRunning=false;
+        } else {
+            IO.println("SAFE SQUARE!");
+        }
+        board.printBoard(false);
+    }
+
+    private void cpuTurn() {
+        IO.println("CPU is playing..");
+        int[] move =cpu.makeMove(board);
+
+        IO.println(String.format("CPU choosed row and colon", move[0],move[1]));
+        if (board.revealCell(move[0], move [1])) {
+            IO.println("CPU got BOMBED!");
+            isRunning = false;
+        } else {
+            IO.println("CPU hits a safe square!");
+        }
+    }
+
+    private boolean checkGameOver() {
+       if (board.allSafeCellsRevealed()) {
+           IO.println("All the safe squares are founden, You Win! ");
+           isRunning = false;
+           return true;
+       }
+       return !isRunning;
+    }
+    private void endGame() {
+        IO.println("Game Over!");
+        highScore.saveScore(player.getName(),
+        board.getRevealedCount());
+    }
+
  }
