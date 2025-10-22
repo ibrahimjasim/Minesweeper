a2d9f52d084d51aa447d6cff2b56e98dedfd9b65
Pinar <pinarbildirici87@gmail.com>
2025-10-22 20:33:14 +0200

    Added Add Player class with name field and getter method"

--- src/Player.java
+++ src/Player.java
@@ -1,1 +1,15 @@
 
+public class Player {
+    private String name;
+
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+}
