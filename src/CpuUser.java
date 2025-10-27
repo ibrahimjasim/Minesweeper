3feb50d68659e2093dd507523b9a14b2c63f6dee
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2025-10-27 21:41:50 +0100

    Added final both string name and rand  to CPUUSER class

--- src/CpuUser.java
+++ src/CpuUser.java
@@ -1,32 +1,31 @@
 import java.util.Random;
 
 // Represents a CPU-controlled player in the Minesweeper game
-
 public class CpuUser {
-    private String name;
-    private Random rand;
+    private final String name;
+    private final Random rand;
 
-}
+    public CpuUser(String name) {
+        this.name = name;
+        this.rand = new Random();
+    }
 
-        public String getName() {
-
+    public String getName() {
         return name;
-        }
+    }
 
-        // The CPU randomly selects a cell that has not been revealed or flagged yet//
-        public int[] makeMove(Bord board) {
-            int rows = board.getRows();
-            int cols = board.getCols();
-            int r, c;
+    // The CPU randomly selects a cell that has not been revealed or flagged yet
+    public int[] makeMove(Board board) {
+        int rows = board.getRows();
+        int cols = board.getCols();
+        int r, c;
 
-            // Randomly pick a cell until an unrevealed and unflagged one is found
-            do {
-                r = rand.nextInt(rows);
-                c = rand.nextInt(cols);
-            } while (board.isRevealed(r, c) || board.isFlagged(r, c));
+        // Randomly pick a cell until an unrevealed and unflagged one is found
+        do {
+            r = rand.nextInt(rows);
+            c = rand.nextInt(cols);
+        } while (board.isRevealed(r, c) || board.isFlagged(r, c));
 
-            return new int[]{r, c};
-        }
+        return new int[]{r, c};
+    }
 }
-
-
