0eb9b5fecf782a963200129ff2c1e423bf261457
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2025-10-28 15:37:05 +0100

    Added spacing and connected the Enter method

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
-
-    public static void main(String[] args) {  // <-- Must be public
+    public static void main(String[] args) {
         IO.println("====== Welcome to Minesweeper ======");
-        IO.println("Created by: Ibrahim, Pinar, and Micke");
-        IO.println("Press Enter to start...");
-        IO.readString("");
+        IO.println("Created by: Ibrahim, Pinar and Micke");
+        System.out.println();
+        IO.waitForEnter("Press ENTER to start...");
+        System.out.println();
 
         GameController controller = new GameController();
         controller.startGame();
 
-        IO.println("====== Thanks for playing! ======");
+        IO.waitForEnter("Press ENTER to exit...");
     }
 }
